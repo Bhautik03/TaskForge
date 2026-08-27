d387cf0a025434716d05b5c9b5f9e63363f538dc
Bhautik Vekariya <bhautikv03@gmail.com>
2026-08-27 12:26:43 +0530

    Add Signal Helper

--- include/signals.h
+++ include/signals.h
@@ -1,6 +1,12 @@
 #ifndef SIGNALS_H
 #define SIGNALS_H
 
-/* Signal handling interface placeholder */
+#include <signal.h>
+
+/* Setup sigaction configuration for process signals */
+void signals_init(void);
+
+/* Sends target signal to specified PID using kill() system call */
+int signals_send(pid_t pid, int sig);
 
 #endif /* SIGNALS_H */
