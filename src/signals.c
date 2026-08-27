76348bf53cda6ea98b11a4035c65f65cdaa243ad
Bhautik Vekariya <bhautikv03@gmail.com>
2026-08-27 12:27:47 +0530

    Implement kill signal wrapper

--- src/signals.c
+++ src/signals.c
@@ -1,7 +1,28 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
 #include "signals.h"
 
-/* 
- * Signals Module Skeleton.
- * Implementation will be added in subsequent development phases.
- */
-typedef int iso_c_guard_signals;
+void signals_init(void)
+{
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    /* Ignore SIGPIPE using sigaction() so broken pipes don't crash scheduler */
+    sigaction(SIGPIPE, &sa, NULL);
+}
+
+int signals_send(pid_t pid, int sig)
+{
+    if (pid <= 0) {
+        errno = EINVAL;
+        return -1;
+    }
+    return kill(pid, sig);
+}
